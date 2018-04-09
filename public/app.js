99a8251a2747ce6f3771302eacfa8b6369920547
Angus Malloch <angusbass@hotmail.com>
2018-04-09 15:48:17 +0100

    Appended child li's to parent ul. Appended catUl to #cats selector.

--- public/app.js
+++ public/app.js
@@ -3,14 +3,23 @@ var app = function() {
     let catUl = document.createElement("ul");
     catUl.classList.add("cat");
 
-    let nameLi1 = document.createElement("li");
-    nameLi1.innerText = 'Name: Tiger';
+    let nameLi = document.createElement("li");
+    nameLi.innerText = 'Name: Tiger';
 
-    let nameLi2 = document.createElement("li");
-    nameLi2.innerText = 'Favourite Food: Mind Alan Cat Food';
+    let foodLi = document.createElement("li");
+    foodLi.innerText = 'Favourite Food: Mind Alan Cat Food';
 
-    let nameLi3 = document.createElement("li");
-    nameLi3.innerHTML = 'img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"';
+    let imageLi = document.createElement("li");
+    imageLi.innerHTML = 'img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"';
+
+    catUl.appendChild(nameLi);
+
+    catUl.appendChild(foodLi);
+
+    catUl.appendChild(imageLi);
+
+    let cats = document.querySelector("#cats");
+    cats.appendChild(catUl);
 
     debugger;
 
