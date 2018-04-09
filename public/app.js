03d551997c39c1f3c59ae770c3357d6bc26ee309
Angus Malloch <angusbass@hotmail.com>
2018-04-09 15:41:59 +0100

    Added li elements for name food and image.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,19 @@
 var app = function() {
 
-  // remember to debugger as you go
-    // step 1 create the parent container and its class
     let catUl = document.createElement("ul");
     catUl.classList.add("cat");
+
+    let nameLi1 = document.createElement("li");
+    nameLi1.innerText = 'Name: Tiger';
+
+    let nameLi2 = document.createElement("li");
+    nameLi2.innerText = 'Favourite Food: Mind Alan Cat Food';
+
+    let nameLi3 = document.createElement("li");
+    nameLi3.innerHTML = 'img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"';
 
     debugger;
 
 ;}
+
+window.onload = app;
