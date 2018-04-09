ea34971addaddf086ada66ac6d218ea839bf0be2
Angus Malloch <angusbass@hotmail.com>
2018-04-09 15:53:49 +0100

    Added functions to create UL and LI for cat name, food and image.

--- public/app.js
+++ public/app.js
@@ -1,28 +1,52 @@
-var app = function() {
+// var app = function() {
+//
+//     let catUl = document.createElement("ul");
+//     catUl.classList.add("cat");
+//
+//     let nameLi = document.createElement("li");
+//     nameLi.innerText = 'Name: Tiger';
+//
+//     let foodLi = document.createElement("li");
+//     foodLi.innerText = 'Favourite Food: Mind Alan Cat Food';
+//
+//     let imageLi = document.createElement("li");
+//     imageLi.innerHTML = 'img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"';
+//
+//     catUl.appendChild(nameLi);
+//
+//     catUl.appendChild(foodLi);
+//
+//     catUl.appendChild(imageLi);
+//
+//     let cats = document.querySelector("#cats");
+//     cats.appendChild(catUl);
+//
+// ;}
 
-    let catUl = document.createElement("ul");
-    catUl.classList.add("cat");
+var createCatUl = function(){
+  let catUl = document.createElement("ul");
+  catUl.classList.add("cat");
+  return catUl;
+}
 
-    let nameLi = document.createElement("li");
-    nameLi.innerText = 'Name: Tiger';
+var createNameLi = function(nameText){
+  let nameLi = document.createElement("li");
+  nameLi.innerText = nameText;
+  return nameLi;
+};
 
-    let foodLi = document.createElement("li");
-    foodLi.innerText = 'Favourite Food: Mind Alan Cat Food';
+var createFoodLi = function(foodText){
+  let foodLi = document.createElement("li");
+  foodLi.innerText = foodText;
+  return foodText;
+};
 
-    let imageLi = document.createElement("li");
-    imageLi.innerHTML = 'img width="500" src="https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"';
+var createImageLi = function(imageText){
+  let imageLi = document.createElement("li");
+  imageLi.innerText = imageText;
+  return imageLi;
+};
 
-    catUl.appendChild(nameLi);
-
-    catUl.appendChild(foodLi);
-
-    catUl.appendChild(imageLi);
-
-    let cats = document.querySelector("#cats");
-    cats.appendChild(catUl);
-
-    debugger;
-
-;}
+debugger;
 
 window.onload = app;
