012a9343361a8c5f4aca8cb64dd0ea3b47a90333
Angus Malloch <angusbass@hotmail.com>
2018-04-09 15:26:54 +0100

    Added variables to select element by id and selector for cat class.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,13 @@
 var app = function() {
 
+    // <section id="cats">
+        // <ul class="cat">
+
     const cats = document.getElementById("cats");
+
+    const cat = document.querySelector("cat")
+
+    const allCat = document.querySelectorAll(".cat");
 
     debugger;
   };
